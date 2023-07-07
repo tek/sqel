b240dbb95eaace6197ca7d97ed7292949e02085c
Torsten Schmits <git@tryp.io>
2023-07-07 23:31:45 +0200

    remove unused cruft

--- packages/core/lib/Sqel/Build/Statement.hs
+++ packages/core/lib/Sqel/Build/Statement.hs
@@ -10,24 +10,6 @@ import Sqel.Data.Statement (Statement (Statement))
 import Sqel.Dd (MaybeDdType)
 import Sqel.Kind.Maybe (MaybeD (JustD, NothingD))
 
--- TODO unused
-
--- type BuildStatementNoQuery :: Type -> [Type] -> Maybe [Type] -> Type -> Constraint
--- class BuildStatementNoQuery tag cs results result where
---   buildStatementNoQuery ::
---     Bool ->
---     Clauses tag cs results a ->
---     Statement () result
-
--- instance (
---     BuildSql tag cs,
---     ClausesResultDecoder results result
---   ) => BuildStatementNoQuery tag cs results result where
---     buildStatementNoQuery multi cs@(Clauses _ res _) =
---       Statement (buildSql @tag multi cs) mempty row
---       where
---         row = (clausesResultDecoder res).decodeValue
-
 type BuildStatement :: ∀ {extq} . Type -> Maybe (DdK extq) -> [Type] -> Maybe [Type] -> Type -> Constraint
 class BuildStatement tag query cs results result where
   buildStatement ::
